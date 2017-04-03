28b571a9242f71d910bbd6b9e830dbbf58c3c26d
Ubuntu <hadoop@adlbigtop1.adlbigtop1.f4.internal.cloudapp.net>
2017-04-03 20:03:24 +0000

    Mahout test class update

--- bigtop-tests/test-artifacts/mahout/src/main/groovy/org/apache/bigtop/itest/mahout/smoke/TestMahoutExamples.groovy
+++ bigtop-tests/test-artifacts/mahout/src/main/groovy/org/apache/bigtop/itest/mahout/smoke/TestMahoutExamples.groovy
@@ -90,19 +90,24 @@ public class TestMahoutExamples {
   @BeforeClass
   public static void setUp() {
     download();
-
+    sh.exec("hadoop fs -mkdir /user/root/testdata");
+    sh.exec("hadoop fs -mkdir /user/root/output");
+    sh.exec("hadoop fs -mkdir /tmp");
+    println sh.out
+    println sh.err
+    println "testdata, output and tmp created download_dir=" + download_dir
     // uncompress archives
-    sh.exec("mkdir ${TEMP_DIR}",
+    sh.exec("mkdir -p ${TEMP_DIR}",
       "cd ${TEMP_DIR}",
-      //Create news-date data dir :: input for classifier test
-      "mkdir 20news-bydate",
+    //Create news-date data dir :: input for classifier test
+      "mkdir -p 20news-bydate",
       "cd 20news-bydate",
       "tar xzf ${download_dir}/20news-bydate.tar.gz",
       "cd ..",
       //Create news-all data directory :: input for LDA test
-      "mkdir 20news-all",
-      "cp -R 20news-bydate/*/* 20news-all",
-      "mkdir reuters-sgm",
+      "mkdir -p 20news-all",
+      "yes | cp -fR 20news-bydate/*/* 20news-all",
+     "mkdir reuters-sgm",
       "cd reuters-sgm",
       "tar xzf ${download_dir}/reuters21578.tar.gz",
       "cd ..",
@@ -110,8 +115,12 @@ public class TestMahoutExamples {
       "mkdir movielens",
       "cd movielens",
       "unzip ${download_dir}/ml-1m.zip");
+    println "beforeclass: sh.out = " + sh.out + " sh.err = " + sh.err;
     assertEquals("Failed to uncompress archives", 0, sh.getRet());
+    println WORK_DIR
     sh.exec("hadoop fs -mkdir ${WORK_DIR}");
+    println sh.out
+    println sh.err
     assertEquals("Unable to create work dir in HCFS", 0, sh.getRet());
     rmr("temp");
   }
@@ -125,14 +134,18 @@ public class TestMahoutExamples {
     //Cat the commands to a central file thats easy to tail.
     //TODO a simpler
     sh.exec("echo \"" + cmd + "\" >> /var/log/mahout.smoke");
+    println "SN: $cmd"
+
     sh.exec(cmd);
+    println "SN: Output - ";
+    println sh.out
     assertEquals("non-zero return! :::: " + cmd + " :::: out= " + sh.out + " :::: err= " + sh.err, 0, sh.getRet());
   }
 
   @AfterClass
   public static void tearDown() {
     sh.exec("rm -rf ${TEMP_DIR}",
-      "hadoop fs -rmr ${WORK_DIR}");
+    "hadoop fs -rmr ${WORK_DIR}");
   }
 
   private static void rmr(String path) {
@@ -162,12 +175,59 @@ public class TestMahoutExamples {
    * This is the full workflow for creating recommendations based on movie
    * ratings including creating training/test data, ALS for training, evaluating
    * the ALS, and then outputting final movie recommendations for users.
+   *     
+   *  To run manually
+   *  Do download and setup steps put above
+   *  assuming TEMP_DIR as /tmp/mahout.1
+   *  STEP 1:
+   *  	cat /tmp/mahout.1/movielens/ml-1m/ratings.dat |sed -e s/::/,/g| cut -d, -f1,2,3 > /tmp/mahout.1/movielens/ratings.csv
+   *  STEP 2:
+   *  	/usr/lib/mahout/bin/mahout splitDataset --input /tmp/mahout.1/movielens/ratings.csv 
+   *    --output /tmp/mahout.1/dataset --trainingPercentage 0.9 --probePercentage 0.1 
+   *    --tempDir /tmp/mahout.1/dataset/tmp
+   *  	//// OUTPUT:
+   *  	//// drwxr-xr-x   - root hdfs          0 2017-03-30 10:27 /tmp/mahout.1/dataset/probeSet
+   *  	//// drwxr-xr-x   - root hdfs          0 2017-03-30 10:26 /tmp/mahout.1/dataset/tmp
+   *  	//// drwxr-xr-x   - root hdfs          0 2017-03-30 10:27 /tmp/mahout.1/dataset/trainingSet
+   *  STEP 3:	
+   *  	/usr/lib/mahout/bin/mahout parallelALS --input /tmp/mahout.1/dataset/trainingSet/ 
+   *    --output /tmp/mahout.1/als/out --tempDir /tmp/mahout.1/als/tmp --numFeatures 20 --numIterations 2 
+   *    --lambda 0.065
+   *  	//// OUTPUT:
+   *  	//// drwxr-xr-x   - root hdfs          0 2017-03-30 10:34 /tmp/mahout.1/als/out/M
+   *  	//// drwxr-xr-x   - root hdfs          0 2017-03-30 10:34 /tmp/mahout.1/als/out/U
+   *  	//// drwxr-xr-x   - root hdfs          0 2017-03-30 10:31 /tmp/mahout.1/als/out/userRatings
+   *  
+   *  STEP 4:
+   *  	/usr/lib/mahout/bin/mahout evaluateFactorization --output /tmp/mahout.1/als/rmse 
+   *    --input /tmp/mahout.1/dataset/probeSet/ --userFeatures /tmp/mahout.1/als/out/U/ 
+   *    --itemFeatures /tmp/mahout.1/als/out/M/ --tempDir /tmp/mahout.1/als/tmp
+   *  	//// OUTPUT:
+   *  	//// -rw-r--r--   1 root hdfs         18 2017-03-30 10:36 /tmp/mahout.1/als/rmse/rmse.txt
+   *  	//cat of rmse.txt => "0.9090948401043087"
+   *  STEP 5: 
+   *  	/usr/lib/mahout/bin/mahout recommendfactorized --input /tmp/mahout.1/als/out/userRatings/ 
+   *    --output /tmp/mahout.1/recommendations --userFeatures /tmp/mahout.1/als/out/U/ 
+   *    --itemFeatures /tmp/mahout.1/als/out/M/ --numRecommendations 6 --maxRating 5
+   *  	//// OUTPUT:
+   *  	//// -rw-r--r--   1 root hdfs          0 2017-03-30 10:53 /tmp/mahout.1/recommendations/_SUCCESS
+   *  	//// -rw-r--r--   1 root hdfs     518929 2017-03-30 10:53 /tmp/mahout.1/recommendations/part-m-00000
+   *  	// hadoop dfs -cat /tmp/mahout.1/recommendations/part-m-00000
+   *  	//	...
+   *  	//	4117    [572:5.0,53:4.7716093,2569:4.7268662,598:4.6782455,745:4.664537,2309:4.658502]
+   *  	//	4118    [858:4.7592306,3338:4.7078934,1221:4.628978,2019:4.621715,2905:4.5735536,922:4.55181]
+   *  	//	4119    [572:5.0,37:4.885694,2197:4.7845206,1545:4.7735705,3853:4.6446133,3092:4.642074]
+   *  	//	...
    */
   @Test(timeout = 12000000L)
   public void factorizeMovieLensRatings() {
     // convert ratings
     sh.exec("cat ${TEMP_DIR}/movielens/ml-1m/ratings.dat |sed -e s/::/,/g| cut -d, -f1,2,3 > ${TEMP_DIR}/movielens/ratings.csv");
     assertEquals("Unexpected error from converting ratings", 0, sh.getRet());
+
+    sh.exec("wc  ${TEMP_DIR}/movielens/ratings.csv");
+    println "factorizeMovieLensRatings: 1 - out: " + sh.out
+    println "factorizeMovieLensRatings: 1 - err: " + sh.err
 
     // put ratings in hdfs
     sh.exec("hadoop fs -mkdir ${WORK_DIR}/movielens",
@@ -178,6 +238,10 @@ public class TestMahoutExamples {
     assertRun("splitDataset --input ${WORK_DIR}/movielens/ratings.csv --output ${WORK_DIR}/dataset " +
       "--trainingPercentage 0.9 --probePercentage 0.1 --tempDir ${WORK_DIR}/dataset/tmp");
 
+     sh.exec("hadoop fs -ls ${WORK_DIR}/dataset");
+     println "factorizeMovieLensRatings: 2 - out: " + sh.out
+     println "factorizeMovieLensRatings: 2 - err: " + sh.err
+
     //Default iterations was 10, but for simple smokes that most might run,
     //2 iterations will confirm enough to move on.
 
@@ -185,27 +249,37 @@ public class TestMahoutExamples {
 
     assertRun("parallelALS --input ${WORK_DIR}/dataset/trainingSet/ --output ${WORK_DIR}/als/out " +
       "--tempDir ${WORK_DIR}/als/tmp --numFeatures 20 --numIterations ${ITERATIONS} --lambda 0.065");
+     sh.exec("hadoop fs -ls ${WORK_DIR}/als/out");
+     println "factorizeMovieLensRatings: 3 - out: " + sh.out
+     println "factorizeMovieLensRatings: 3 - err: " + sh.err
 
-    //remove this
-    sh.exec("hadoop fs -ls ${WORK_DIR}/als/out >> /tmp/mahoutdebug");
     //compute predictions against the probe set, measure the error
     assertRun("evaluateFactorization --output ${WORK_DIR}/als/rmse --input ${WORK_DIR}/dataset/probeSet/ " +
       "--userFeatures ${WORK_DIR}/als/out/U/ --itemFeatures ${WORK_DIR}/als/out/M/ --tempDir ${WORK_DIR}/als/tmp");
+     sh.exec("hadoop fs -ls ${WORK_DIR}/als/rmse");
+     println "factorizeMovieLensRatings: 4 - out: " + sh.out
+     println "factorizeMovieLensRatings: 4 - err: " + sh.err
 
     //compute recommendations
     assertRun("recommendfactorized --input ${WORK_DIR}/als/out/userRatings/ --output ${WORK_DIR}/recommendations " +
       "--userFeatures ${WORK_DIR}/als/out/U/ --itemFeatures ${WORK_DIR}/als/out/M/ " +
       "--numRecommendations 6 --maxRating 5");
+     sh.exec("hadoop fs -ls ${WORK_DIR}/recommendations");
+     println "factorizeMovieLensRatings: 5 - out: " + sh.out
+     println "factorizeMovieLensRatings: 5 - err: " + sh.err
 
     // check that error has been calculated
     assertEquals("${WORK_DIR}/als/rmse/rmse.txt does not exist", 0, sh.getRet());
-    // print the error
-    sh.exec("hadoop fs -cat ${WORK_DIR}/als/rmse/rmse.txt");
     assertEquals("Unexpected error from running hadoop", 0, sh.getRet());
 
     // check that recommendations has been calculated
     sh.exec("hadoop fs -test -e ${WORK_DIR}/recommendations/part-m-00000");
     assertEquals("${WORK_DIR}/recommendations/part-m-00000 does not exist", 0, sh.getRet());
+
+    sh.exec("hadoop dfs -cat ${WORK_DIR}/recommendations/part-m-00000");
+    println "factorizeMovieLensRatings: 5 - out: " + sh.out
+    println "factorizeMovieLensRatings: 5 - err: " + sh.err
+
   }
 
   /**
@@ -221,6 +295,8 @@ public class TestMahoutExamples {
    * versus places which have cyclic performance (i.e. due to weather).
    */
   private void _clusterSyntheticControlData(String algorithm) {
+    println("**SN: Creating testdata");
+    sh.exec("echo **SN:  Creating testdata");
     rmr("testdata");
     sh.exec("hadoop fs -mkdir testdata",
       "hadoop fs -put ${download_dir}/synthetic_control.data testdata");
@@ -236,6 +312,8 @@ public class TestMahoutExamples {
 
   @Test(timeout = 9000000L)
   public void clusterControlDataWithKMeans() {
+    println("**SN: kmeans");
+    sh.exec("**SN: kmeans");
     _clusterSyntheticControlData("kmeans");
   }
 
@@ -244,15 +322,38 @@ public class TestMahoutExamples {
     _clusterSyntheticControlData("fuzzykmeans");
   }
 
+  @Test
+  public void TestDateTimeTest() {
+
+    def m1 = System.currentTimeMillis()
+     println m1
+
+     def m2 = new Date()
+     println m2.getTime()
+     println "TestSN: " + sh.exec("hadoop fs -ls /");
+     println "TestSN: sh.out=" + sh.out;
+     println "TestSN: sh.err=" + sh.err;
+     println "TestSN: sh.getRet()=" + sh.getRet();
+  }
+
   /**
    * Test the creation of topical clusters from raw lists words using LDA.
    */
-  @Test(timeout = 7200000L)
+   @Test(timeout = 72000000L)
   public void testReutersLDA() {
     // where does lda.algorithm come in?
+    println "***SN: testReutersLDA - 1"
+
+    def m1 = System.currentTimeMillis()
+     println m1
+
+     def m2 = new Date()
+     println m2.getTime()
     assertRun("org.apache.lucene.benchmark.utils.ExtractReuters ${TEMP_DIR}/reuters-sgm ${TEMP_DIR}/reuters-out");
+    println "***SN: testReutersLDA - 2"
     //put ${TEMP_DIR}/reuters-out into hdfs as we have to run seqdirectory in mapreduce mode, so files need be in hdfs
     sh.exec("hadoop fs -put ${TEMP_DIR}/reuters-out ${WORK_DIR}/reuters-out");
+    println "***SN: testReutersLDA - 3"
     assertEquals("Unable to put reuters-out-seqdir in hdfs", 0, sh.getRet());
 
     assertRun("seqdirectory -i ${TEMP_DIR}/reuters-out -o ${TEMP_DIR}/reuters-out-seqdir -c UTF-8 -chunk 5");
@@ -280,16 +381,30 @@ mahout ldatopics \
 -i ${WORK_DIR}/reuters-lda/state-20 \
 -d ${WORK_DIR}/reuters-out-seqdir-sparse-lda/dictionary.file-0 \
 -dt sequencefile""");
+
+   sh.exec("hadoop dfs -ls ${WORK_DIR}/reuters-lda");
+   println "testReutersLDA: reuters-lda: Output" + sh.out
+   println "testReutersLDA: reuters-lda: Err" + sh.err
   }
 
   /**
    * Note that this test doesnt work on some older mahout versions.
    */
-  @Test(timeout = 9000000L)
+  @Test(timeout = 90000000L)
   public void testBayesNewsgroupClassifier() {
     // put bayes-train-input and bayes-test-input in hdfs
+/*     sh.exec("hadoop fs -rm -r /tmp/mahout.1/20news-seq");
+     sh.exec("hadoop fs -rm -r /tmp/mahout.1/20news-train-vectors");
+     sh.exec("hadoop fs -rm -r /tmp/mahout.1/model");
+     sh.exec("hadoop fs -rm -r /tmp/mahout.1/20news-testing");
+
+     println "testBayesNewsgroupClassifier: log: sh.out = " + sh.out + " sh.err = " + sh.err;
+*/
+ 
+    println "start testBayesNewsgroupClassifier"
     sh.exec("hadoop fs -mkdir ${WORK_DIR}/20news-vectors");
     sh.exec("hadoop fs -put ${TEMP_DIR}/20news-all ${WORK_DIR}/20news-all");
+    println "testBayesNewsgroupClassifier: log: sh.out = " + sh.out + " sh.err = " + sh.err;
     assertEquals("Unable to put bayes-train-input in hdfs", 0, sh.getRet());
 
     assertRun("seqdirectory -i ${WORK_DIR}/20news-all -o ${WORK_DIR}/20news-seq");
@@ -321,5 +436,9 @@ mahout ldatopics \
 -m ${WORK_DIR}/model \
 -l ${WORK_DIR}/labelindex \
 -ow -o ${WORK_DIR}/20news-testing""");
+    
+     sh.exec("hadoop fs -ls ${WORK_DIR}/20news-testing");
+     println "testBayesNewsgroupClassifier: out: " + sh.out
+     println "testBayesNewsgroupClassifier: err: " + sh.err
   }
 }
